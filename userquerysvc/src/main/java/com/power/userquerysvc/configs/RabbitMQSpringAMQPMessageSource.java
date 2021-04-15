d418e320390324fe6e5e1a58d77288fd77a4b342
Muhammad Yauri Attamimi <yauritux@Muhammads-MacBook-Pro.local>
2021-04-16 03:02:31 +0700

    add rabbit listener

--- userquerysvc/src/main/java/com/power/userquerysvc/configs/RabbitMQSpringAMQPMessageSource.java
+++ userquerysvc/src/main/java/com/power/userquerysvc/configs/RabbitMQSpringAMQPMessageSource.java
@@ -1,2 +1,28 @@
-package com.power.userquerysvc.configs;public class RabbitMQSpringAMQPMessageSource {
+package com.power.userquerysvc.configs;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.extensions.amqp.eventhandling.AMQPMessageConverter;
+import org.axonframework.extensions.amqp.eventhandling.spring.SpringAMQPMessageSource;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("rabbitMQSpringAMQPMessageSource")
+@Slf4j
+public class RabbitMQSpringAMQPMessageSource extends SpringAMQPMessageSource {
+
+    @Autowired
+    public RabbitMQSpringAMQPMessageSource(final AMQPMessageConverter messageConverter) {
+        super(messageConverter);
+    }
+
+    @RabbitListener(queues = "userEvents.topic.userEvents")
+    @Override
+    public void onMessage(final Message message, final Channel channel) {
+        log.debug("received message: message={}, channel={}", message, channel);
+        System.out.println("Received message = " + message.toString());
+        super.onMessage(message, channel);
+    }
 }
--- userquerysvc/src/main/java/com/power/userquerysvc/handlers/UserEventHandlerImpl.java
+++ userquerysvc/src/main/java/com/power/userquerysvc/handlers/UserEventHandlerImpl.java
@@ -3,12 +3,14 @@ package com.power.userquerysvc.handlers;
 import com.power.usercore.events.UserRegisteredEvent;
 import com.power.userquerysvc.projections.DefaultUserView;
 import com.power.userquerysvc.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
-//@ProcessingGroup("user-group")
+@ProcessingGroup("userEvents")
 public class UserEventHandlerImpl implements UserEventHandler {
 
     private final UserRepository userRepository;
@@ -20,6 +22,8 @@ public class UserEventHandlerImpl implements UserEventHandler {
     @EventHandler
     @Override
     public void on(UserRegisteredEvent event) {
+        log.debug("processing UserRegisteredEvent={}", event);
+        System.out.println("Processing UserRegisteredEvent->" + event.toString());
         var userView = DefaultUserView.builder()
                 .id(event.getId())
                 .firstName(event.getFirstName())
