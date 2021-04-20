1ff5f74f555c65b9ba38897aaffd0b39770b8fe7
Muhammad Yauri Attamimi <yauritux@Muhammads-MacBook-Pro.local>
2021-04-20 17:46:41 +0700

    update user command

--- usercmdsvc/src/main/java/com/power/usercmdsvc/commands/UpdateUserCommand.java
+++ usercmdsvc/src/main/java/com/power/usercmdsvc/commands/UpdateUserCommand.java
@@ -1,2 +1,19 @@
-package com.power.usercmdsvc.commands;public class UpdateUserCommand {
+package com.power.usercmdsvc.commands;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserCommand {
+    @TargetAggregateIdentifier
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
