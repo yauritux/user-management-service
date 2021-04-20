830fd8c0525705c17b75e35fcfa007d53cee6efa
Muhammad Yauri Attamimi <yauritux@Muhammads-MacBook-Pro.local>
2021-04-21 00:56:23 +0700

    add email lookup entity to be used as our set based consistency validation

--- usercmdsvc/src/main/java/com/power/usercmdsvc/repositories/EmailLookupRepository.java
+++ usercmdsvc/src/main/java/com/power/usercmdsvc/repositories/EmailLookupRepository.java
@@ -1,2 +1,12 @@
-package com.power.usercmdsvc.repositories;public interface EmailLookupRepository {
+package com.power.usercmdsvc.repositories;
+
+import com.power.usercmdsvc.entities.EmailLookupEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailLookupRepository extends JpaRepository<EmailLookupEntity, String> {
+
+    Optional<EmailLookupEntity> findByUsername(final String username);
+    Optional<EmailLookupEntity> findByEmail(final String email);
 }
