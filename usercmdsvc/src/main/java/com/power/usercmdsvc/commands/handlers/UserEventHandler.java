7a6cff9b0537b02dd7a252d4731b063fe8b2c461
Muhammad Yauri Attamimi <yauritux@Muhammads-MacBook-Pro.local>
2021-04-21 00:58:45 +0700

    implement event handler at the command side to update our lookup table

--- usercmdsvc/src/main/java/com/power/usercmdsvc/commands/handlers/UserEventHandler.java
+++ usercmdsvc/src/main/java/com/power/usercmdsvc/commands/handlers/UserEventHandler.java
@@ -1,2 +1,23 @@
-package com.power.usercmdsvc.commands.handlers;public class UserEventHandler {
+package com.power.usercmdsvc.commands.handlers;
+
+import com.power.usercmdsvc.entities.EmailLookupEntity;
+import com.power.usercmdsvc.repositories.EmailLookupRepository;
+import com.power.usercmdsvc.services.UserStreamingService;
+import com.power.usercore.events.UserRegisteredEvent;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@ProcessingGroup("emailEntity")
+public class UserEventHandler {
+
+    @EventHandler
+    public void on(UserRegisteredEvent event, EmailLookupRepository emailLookupRepository,
+            UserStreamingService userStreamingService) {
+        emailLookupRepository.save(
+                EmailLookupEntity.builder().email(event.getEmail()).username(event.getUsername())
+                .build());
+        userStreamingService.publishUserRegisteredEvent(event);
+    }
 }
