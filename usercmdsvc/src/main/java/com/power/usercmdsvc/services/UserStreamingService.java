407964a1aade3f47536366e27e5bdd9159c5a8f8
Muhammad Yauri Attamimi <yauritux@Muhammads-MacBook-Pro.local>
2021-04-20 17:48:44 +0700

    add new method to publish user update event

--- usercmdsvc/src/main/java/com/power/usercmdsvc/services/UserStreamingService.java
+++ usercmdsvc/src/main/java/com/power/usercmdsvc/services/UserStreamingService.java
@@ -1,7 +1,9 @@
 package com.power.usercmdsvc.services;
 
 import com.power.usercore.events.UserRegisteredEvent;
+import com.power.usercore.events.UserUpdatedEvent;
 
 public interface UserStreamingService {
     void publishUserRegisteredEvent(UserRegisteredEvent event);
+    void publishUserUpdatedEvent(UserUpdatedEvent event);
 }
