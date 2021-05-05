62c781fc331f62516176f9fb99efae8a1bed34b5
Muhammad Yauri Attamimi <yauritux@192.168.1.9>
2021-05-05 18:33:28 +0700

    concurrently call user ad service to create user entry in Azure AD

--- userquerysvc/src/main/java/com/power/userquerysvc/handlers/UserRegisteredEventHandler.java
+++ userquerysvc/src/main/java/com/power/userquerysvc/handlers/UserRegisteredEventHandler.java
@@ -4,27 +4,38 @@ import com.google.gson.Gson;
 import com.power.usercore.events.UserRegisteredEvent;
 import com.power.userquerysvc.projections.DefaultUserView;
 import com.power.userquerysvc.repositories.UserRepository;
+import com.power.userquerysvc.services.UserADService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserRegisteredEventHandler implements UserEventHandler {
 
     private final UserRegisteredEvent event;
     private final ApplicationContext applicationContext;
+    private final UserADService userADService;
 
-    public UserRegisteredEventHandler(String payload, ApplicationContext applicationContext) {
+    public UserRegisteredEventHandler(String payload, ApplicationContext applicationContext, UserADService userADService) {
         this.event = new Gson().fromJson(payload, UserRegisteredEvent.class);
         this.applicationContext = applicationContext;
+        this.userADService = userADService;
     }
 
     @Override
     public void handle() {
         var userRepository = (UserRepository) applicationContext.getBean("userRepository");
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         var userView = DefaultUserView.builder()
                 .id(event.getId())
                 .firstName(event.getFirstName())
                 .lastName(event.getLastName())
                 .username(event.getUsername())
-                .email(event.getEmail()).build();
+                .password(passwordEncoder.encode(event.getPassword()))
+                .email(event.getEmail())
+                .role(event.getRole())
+                .build();
+        System.out.println("Persisting UserView into user read database...");
         userRepository.save(userView);
+        System.out.println("Adding new record into our user active directory...");
+        userADService.createUser(userView);
     }
 }
