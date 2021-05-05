06f5df8e7963868c44fc3924391c157369f93680
Muhammad Yauri Attamimi <yauritux@192.168.1.9>
2021-05-05 18:31:48 +0700

    setup security config

--- userquerysvc/src/main/java/com/power/userquerysvc/configs/SecurityConfig.java
+++ userquerysvc/src/main/java/com/power/userquerysvc/configs/SecurityConfig.java
@@ -1,2 +1,14 @@
-package com.power.userquerysvc.configs;public class SecurityConfig {
+package com.power.userquerysvc.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
 }
