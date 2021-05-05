1b4eb544ef9d603dae3b2a6cf2982332c7915773
Muhammad Yauri Attamimi <yauritux@192.168.1.9>
2021-05-05 18:33:41 +0700

    inject user ad service

--- userquerysvc/src/main/java/com/power/userquerysvc/handlers/UserUpdatedEventHandler.java
+++ userquerysvc/src/main/java/com/power/userquerysvc/handlers/UserUpdatedEventHandler.java
@@ -3,16 +3,19 @@ package com.power.userquerysvc.handlers;
 import com.google.gson.Gson;
 import com.power.usercore.events.UserUpdatedEvent;
 import com.power.userquerysvc.repositories.UserRepository;
+import com.power.userquerysvc.services.UserADService;
 import org.springframework.context.ApplicationContext;
 
 public class UserUpdatedEventHandler implements UserEventHandler {
 
     private final UserUpdatedEvent event;
     private final ApplicationContext applicationContext;
+    private final UserADService userADService;
 
-    public UserUpdatedEventHandler(String payload, ApplicationContext applicationContext) {
+    public UserUpdatedEventHandler(String payload, ApplicationContext applicationContext, UserADService userADService) {
         this.event = new Gson().fromJson(payload, UserUpdatedEvent.class);
         this.applicationContext = applicationContext;
+        this.userADService = userADService;
     }
 
     @Override
